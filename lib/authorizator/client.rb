519d565475e6915ea31f0b73a96d7f12bd0becc3
Lorenzo Tello <ltello8a@gmail.com>
2014-04-09 18:40:57 +0200

    raise error when unable to obtain a valid access token

--- lib/authorizator/client.rb
+++ lib/authorizator/client.rb
@@ -43,7 +43,7 @@ module Authorizator
     def services
       maybe_renewing_access_token do
         access_token.get(services_endpoint)
-      end.parsed
+      end.parsed['services']
     end
 
 
@@ -53,7 +53,10 @@ module Authorizator
       #
       # @returns [OAuth2::AccessToken] instance through which access the Authorizator service api endpoints.
       def access_token
-        @access_token ||= client_application.client_credentials.get_token(:scope => SCOPE_FOR_A_SERVICE_TO_TALK_TO_AUTHORIZATOR_SERVICE)
+        return @access_token if instance_variable_defined?(:@access_token)
+        new_token = client_application.client_credentials.get_token(:scope => SCOPE_FOR_A_SERVICE_TO_TALK_TO_AUTHORIZATOR_SERVICE)
+        raise("Got invalid access token: #{new_token.params}") if (!new_token or new_token.token.empty?)
+        @access_token = new_token
       end
 
       # An oauth client for a service to oauth-dialog with the Authorizator service. A new one if none was previously created or
@@ -76,12 +79,23 @@ module Authorizator
       # @returns what block returns. Usually an [OAuth2::Response] instance.
       def maybe_renewing_access_token(&block)
         resp = block.call
-        if (resp.respond_to?(:error) and resp.error and AUTHORIZATOR_SERVICE_INVALID_ACCESS_TOKEN_ERROR_CODES.include?(resp.error.code))
+        if invalid_remote_reponse?(resp)
           @access_token = nil
           resp = block.call
+          error = invalid_remote_reponse?(resp)
+          raise(error) if error
         end
         resp
       end
+
+        def invalid_remote_reponse?(resp)
+          return false unless (resp.respond_to?(:error) and resp.error)
+          return resp.error.code if AUTHORIZATOR_SERVICE_INVALID_ACCESS_TOKEN_ERROR_CODES.include?(resp.error.code)
+          AUTHORIZATOR_SERVICE_INVALID_ACCESS_TOKEN_ERROR_CODES.each do |code|
+            return code if resp.headers['www-authenticate'] =~ Regexp.new(code)
+          end
+          false
+        end
 
       # The relative path of the Authorizator service url that gives you a valid talking token.
       #
