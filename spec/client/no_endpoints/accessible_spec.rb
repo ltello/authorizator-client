c203b3ff4b8c836b081b8436f3ebe59bbabd9474
Lorenzo Tello <ltello8a@gmail.com>
2014-05-28 17:21:42 +0200

    add tests to check new #accessible no-endpoint method

---
+++ spec/client/no_endpoints/accessible_spec.rb
@@ -0,0 +1,51 @@
+shared_context '#accessible:' do
+
+  context '#accessible:' do
+    let(:services_endpoint_path)    {'/services'}
+    let(:valid_service1_name_value) {'service_one'}
+    let(:valid_service2_name_value) {'service_two'}
+    let(:valid_service1_site_value) {'https://service1.ideas4all.com'}
+    let(:valid_service2_site_value) {'https://service2.ideas4all.com'}
+    let(:valid_services_data)       {{'services' => [{'name' => valid_service1_name_value, 'site' => valid_service1_site_value},
+                                                     {'name' => valid_service2_name_value, 'site' => valid_service2_site_value}]}}
+
+    shared_examples "an invalid data response..." do |title|
+      it title do
+        allow(access_token).to receive(:get).with(services_endpoint_path).and_return(subject)
+        allow(subject).to receive(:parsed).and_return(subject)
+        expect {authorizator_client.services}.to raise_error(Authorizator::Client::Endpoints::Response::Error::Data)
+      end
+    end
+
+    it 'returns true when the Autorizator host is reachable.' do
+      allow(authorizator_client).to receive(:access_token).and_return(access_token)
+      allow(access_token).to receive(:get).with(services_endpoint_path).and_return(valid_services_data)
+      allow(valid_services_data).to receive(:parsed).and_return(valid_services_data)
+      expect(authorizator_client).to             respond_to(:accessible?)
+      expect(authorizator_client.accessible?).to be(true)
+    end
+
+    context 'And false in any other case:' do
+      let(:blank_authorizator_service_site)       {""}
+      let(:no_protocol_authorizator_service_site) {"localhost:3000"}
+      let(:working_no_authorizator_service_site)  {"http://www.google.com"}
+
+      it "- when the Authorizator's host url is blank?," do
+        allow(authorizator_service).to receive(:site).and_return(blank_authorizator_service_site)
+        expect(authorizator_client.accessible?).to be(false)
+      end
+
+      it "- when the Authorizator's host url has no protocol included," do
+        allow(authorizator_service).to receive(:site).and_return(no_protocol_authorizator_service_site)
+        expect(authorizator_client.accessible?).to be(false)
+      end
+
+      it "- when the Authorizator's host url is not the right one," do
+        allow(authorizator_service).to receive(:site).and_return(working_no_authorizator_service_site)
+        expect(authorizator_client.accessible?).to be(false)
+      end
+    end
+
+  end
+
+end
--- spec/client/public_interface_spec.rb
+++ spec/client/public_interface_spec.rb
@@ -1,3 +1,6 @@
+require 'client/no_endpoints/accessible_spec'
+
+
 shared_context 'public interface' do
 
   describe 'Beside endpoints methods that return data from an associated endpoint, other method can be part of the public
@@ -9,20 +12,25 @@ shared_context 'public interface' do
     let(:authorizator_service_site)      {'http://localhost:3000'}
     let(:authorizator_service)           {service_class.new(nil, nil, authorizator_service_site)}
     let(:authorizator_client)            {Authorizator::Client.new(caller_service:caller_service, authorizator_service:authorizator_service)}
+    let(:access_token)                   {double}
 
-    context 'The authorizator-client gem does not define no-endpoints public interface methods.' do
-      it "However, any future one can be defined in the Authorizator::Client::PublicInterface module and will be
-          available to the gem's calling software" do
-        expect(authorizator_client).not_to respond_to(:new_public_interface_method)
-        Authorizator::Client::PublicInterface.class_eval do
-          define_method :new_public_interface_method do
-            :in_new_public_interface_method
-          end
-        end
-        expect(authorizator_client).to respond_to(:new_public_interface_method)
-        expect(authorizator_client.new_public_interface_method).to eq(:in_new_public_interface_method)
-      end
+    context 'The authorizator-client gem exposes the following no-endpoint methods:' do
+      include_context '#accessible:'
     end
+
+    # context 'The authorizator-client gem does not define no-endpoints public interface methods.' do
+    #   it "However, any future one can be defined in the Authorizator::Client::PublicInterface module and will be
+    #       available to the gem's calling software" do
+    #     expect(authorizator_client).not_to respond_to(:new_public_interface_method)
+    #     Authorizator::Client::PublicInterface.class_eval do
+    #       define_method :new_public_interface_method do
+    #         :in_new_public_interface_method
+    #       end
+    #     end
+    #     expect(authorizator_client).to respond_to(:new_public_interface_method)
+    #     expect(authorizator_client.new_public_interface_method).to eq(:in_new_public_interface_method)
+    #   end
+    # end
   end
 
 end
