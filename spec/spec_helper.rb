44b2dac7de80a3098dba4f65f52d67846b55f423
Lorenzo Tello <ltello8a@gmail.com>
2014-05-28 17:22:03 +0200

    clean up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,8 +13,8 @@
 # The `.rspec` file also contains a few flags that are not defaults but that
 # users commonly want.
 
-
 require 'authorizator/client'
+
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
